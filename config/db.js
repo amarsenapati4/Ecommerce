14eb567c485e74730bc1d427bbdb260ad6f133d2
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2023-06-21 03:46:27 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ import colors from 'colors';
 const connectDB=async()=>{
   try {
     const conn=await mongoose.connect(process.env.MONGO_URl);
-    console.log(`Connecting to MongoDB Database${conn.connection.host}`);
+    console.log(`Connecting to MongoDB Database${conn.connection.host}`.bgMagenta.white);
   } catch (error) {
     console.log(`Error in Mongodb ${error}.bgRed.white`);
   }
