bb2725a85c3952e11461ae008784ee790775b31d
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2023-06-22 00:05:08 +0530

    header change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,42 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {NavLink} from 'react-router-dom'
 import logo from '../img/REXdex copy.png'
 import { FaShoppingCart} from "react-icons/fa";
+import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
+
 
 const Header =()=>{
-    return (
-        <>
-        
-<nav className="fixed top-0 left-0 bg-lime-300 w-full shadow-2xl">
-  <div className="container m-auto flex justify-between items-center text-gray-700">
-    <img src={logo} alt={"logo"} className='pl-8 py-4 h-[5rem]'/>
-     <ul className='hidden md:flex items-center pr-10 text-base font-semibold cursor-pointer uppercase text-[18px] leading-6 font-sans'>
-      <NavLink to="/" className="hover:bg-gray-200 py-4 px-6 font-bold">Home</NavLink>
-      <NavLink to="/Category" className="hover:bg-gray-200 py-4 px-6">Category</NavLink>
-      <NavLink to="/register" className="hover:bg-gray-200 py-4 px-6">Register</NavLink>
-      <NavLink to="/login" className="hover:bg-gray-200 py-4 px-6">Login</NavLink>
-      <NavLink to="/cart" className="hover:bg-gray-200 py-4 px-6 flex justify-center items-center"><FaShoppingCart/>(0)</NavLink>
-     </ul>
-     <button className='block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-gray-200 group'>
-      <div className='w-5 h-1 bg-gray-600 mb-1'></div>
-      <div className='w-5 h-1 bg-gray-600 mb-1'></div>
-      <div className='w-5 h-1 bg-gray-600 mb-1'></div>
-      <div className='absolute top-0 right-full h-screen w-8/12 bg-white shadow-2xl  border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300'>
-        <ul className='flex flex-col items-center w-full  text-base cursor-pointer pt-10 uppercase text-[18px] leading-6 font-sans'>
-        <NavLink to="/" className="hover:bg-gray-200 py-4 px-6 w-full">Home</NavLink>
-        <NavLink to="/Category" className="hover:bg-gray-200 py-4 px-6 w-full">Category</NavLink>
-      <NavLink to="/register" className="hover:bg-gray-200 py-4 px-6 w-full">Register</NavLink>
-      <NavLink to="/login" className="hover:bg-gray-200 py-4 px-6 w-full">Login</NavLink>
-      <NavLink to="/cart" className="hover:bg-gray-200 py-4 px-6 w-full flex justify-center items-center text-[18px]"><FaShoppingCart/>(0)</NavLink>
+  let Links =[
+    {name:"HOME",link:"/home"},
+    {name:"Category",link:"/category"},
+    {name:"Register",link:"/register"},
+    {name:"Login",link:"/login"},
+  ];
+  let [open, setOpen] =useState(false);
+
+return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+       <div className='md:flex items-center justify-between bg-lime-300 py-4 md:px-10 px-7'>
+       <img src={logo} alt={"logo"} className='pl-8 py-4 h-[5rem]'/>
+        {/* Menu icon */}
+        <div onClick={()=>setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
+            {
+                open ? <XMarkIcon/> : <Bars3BottomRightIcon />
+            }
+        </div>
+        {/* linke items */}
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-lime-300 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-13': 'top-[-490px]'}`}>
+            {
+                Links.map((link) => (
+                <li className='md:ml-8 md:my-0 my-7 font-semibold'>
+                    <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
+                </li>))
+               
+            }
+             <NavLink to="/cart" className='md:ml-8 md:my-0 my-7 font-semibold flex'><FaShoppingCart/><span >(0)</span></NavLink>
         </ul>
-
-      </div>
-     </button>
-  </div>
-</nav>
-
-        
-        </>
-    );
+        {/* button */}
+       </div>
+    </div>
+);
 };
 export default Header;
