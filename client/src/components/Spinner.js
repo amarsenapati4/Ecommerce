cf72e875dc1da25fe897a0a8949fe6c2dca98f79
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2023-06-26 09:54:05 +0530

    admin product panel upadate

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 const Spinner = ({ path = "login" }) => {
-    const [count,setCount]=useState(5);
+    const [count,setCount]=useState(3);
     const navigate =useNavigate();
     const location =useLocation();
 
