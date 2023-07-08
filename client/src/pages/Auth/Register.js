b75ed72562e57299335a71fa56ca71926456b6b9
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2023-07-08 17:20:23 +0530

    registration page ui update

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -34,149 +34,69 @@ const Register = () => {
   };
   return (
     <Layout title="Register Ecommerce app">
-      <div className="flex justify-center h-[70vh] bg-transparent items-center flex-col">
-        {/* component */}
-        
-          <div className="flex md:w-1/2  justify-center py-10 mt-[12rem] items-center">
-            <form className="bg-white mb-[2rem] " onSubmit={handelSubmit} >
-              <h1 className="text-gray-800 font-bold text-2xl mb-1 flex items-center justify-center">
-                Registration Page
-              </h1>
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <input
-                  className="pl-2 outline-none border-none"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  type="text"
-                  name
-                  id
-                  placeholder="Full name"
-                  required
-                />
+<div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center mt-[3rem]">
+  <div className="container max-w-screen-lg mx-auto">
+    <div>
+      
+      <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+        <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
+          <div className="text-gray-600">
+            <p className="font-medium text-lg">Sign Up Page</p>
+            <p>Please fill out all the fields.</p>
+          </div>
+          <div className="lg:col-span-2">
+            <form className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5" onSubmit={handelSubmit} >
+              <div className="md:col-span-5">
+                <label htmlFor="full_name">Full Name</label>
+                <input  value={name}
+                  onChange={(e) => setName(e.target.value)} type="text" name="full_name" id="full_name" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="Enter Your Name"
+                  required />
               </div>
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                  />
-                </svg>
-                <input
-                  className="pl-2 outline-none border-none"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  name
-                  id
-                  placeholder="Email Address"
-                  required
-                />
+              <div className="md:col-span-5">
+                <label htmlFor="email">Email Address</label>
+                <input  value={email}
+                  onChange={(e) => setEmail(e.target.value)} type="text" name="email" id="email" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"  placeholder="Email" />
               </div>
-
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <input
-                  className="pl-2 outline-none border-none"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  name
-                  id
-                  placeholder="Password"
-                  required
-                />
+              <div className="md:col-span-5">
+                <label htmlFor="password">Password</label>
+                <input  value={password}
+                  onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="Password"
+                  required/>
               </div>
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 512 512"
-                >
-                  <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
-                </svg>
-                <input
-                  className="pl-2 outline-none border-none"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  type="text"
-                  name
-                  id
-                  placeholder="Phone"
-                  required
-                />
+              <div className="md:col-span-3">
+              <label htmlFor="address">Address</label>
+                <input  value={address}
+                  onChange={(e) => setAddress(e.target.value)} type="text" name="address" id="address" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50" placeholder="address"
+                  required/>
               </div>
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 384 512"
-                >
-                  <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-                </svg>
-                <input
-                  className="pl-2 outline-none border-none"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  type="text"
-                  name
-                  id
-                  placeholder="address"
-                  required
-                />
+              <div className="md:col-span-2">
+              <label htmlFor="Phone">Phone</label>
+                <input   value={phone}
+                  onChange={(e) => setPhone(e.target.value)} type="text" name="Phone" id="Phone" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"   placeholder="Phone"
+                  required/>
               </div>
-              <div className="flex items-center border-2 py-2 px-3 rounded-2xl text-[9px]">
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>
-                <input
-                  className="pl-2 outline-none border-none"
-                  value={answer}
-                  onChange={(e) => setAnswer(e.target.value)}
-                  type="text"
-                  name
-                  id
-                  placeholder="What is name of your pet?"
-                  required
-                />
+            
+              <div className="md:col-span-5">
+              <label htmlFor="security">Security Question</label>
+                <input value={answer}
+                  onChange={(e) => setAnswer(e.target.value)} type="text" name="security" id="security" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"  placeholder="What is name of your pet?"
+                  required/>
               </div>
-              <button
-                type="submit"
-                className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
-              >
-                Submit
-              </button>
+              
+            
+              <div className="md:col-span-5 text-right">
+                <div className="inline-flex items-end">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+                </div>
+              </div>
             </form>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+
     </Layout>
   );
 };
