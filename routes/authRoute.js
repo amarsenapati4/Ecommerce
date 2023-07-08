771ac80f694b5e8031473ee4af0c49740c142168
Susmita Subudhi <susmitasubudhi13@gmail.com>
2023-07-08 17:33:10 +0530

    auth route update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -26,5 +26,7 @@ router.get("/user-auth", requireSignIn, (req, res) => {
 router.get("/admin-auth", requireSignIn,isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
 
 export default router;
