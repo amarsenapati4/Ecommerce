3dd94aea8b58d4fef59d96535db537d3d1918783
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2023-07-08 17:32:12 +0530

    user profile update button ui change

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -114,9 +114,7 @@ const Profile = () => {
                   />
                 </div>
 
-                <button type="submit" className="btn btn-primary">
-                  UPDATE
-                </button>
+                <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">UPDATE</button>
               </form>
             </div>
           </div>
