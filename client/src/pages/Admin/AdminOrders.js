32af52ecfdeb0bcf30f9e64cd344bbf0f5ee2b4e
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2023-07-17 21:59:07 +0530

    admin and user order page design

--- client/src/pages/Admin/AdminOrders.js
+++ client/src/pages/Admin/AdminOrders.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
-import AdminMenu from "../../components/Layout/AdminMenu";
+import AdminMenu from "../../components/Layout/AdminMenue";
 import Layout from "../../components/Layout/Layout";
 import { useAuth } from "../../Context/auth";
 import moment from "moment";
@@ -44,31 +44,47 @@ const AdminOrders = () => {
   };
   return (
     <Layout title={"All Orders Data"}>
-      <div className="row dashboard">
-        <div className="col-md-3">
+      <div className="container-fluid mt-[6rem] flex md:flex-row flex-col">
+        <div className="col-md-3 flex flex-row">
           <AdminMenu />
         </div>
-        <div className="col-md-9">
+        <div className="col-md-9 mt-4 md:mt-0">
           <h1 className="text-center">All Orders</h1>
           {orders?.map((o, i) => {
             return (
               <div className="border shadow">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Status</th>
-                      <th scope="col">Buyer</th>
-                      <th scope="col"> date</th>
-                      <th scope="col">Payment</th>
-                      <th scope="col">Quantity</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{i + 1}</td>
-                      <td>
-                        <Select
+               <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    #
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Buyer
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Payment
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Quantity
+                </th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {i+1}
+                </th>
+                <td class="px-6 py-4">
+<Select
                           bordered={false}
                           onChange={(value) => handleChange(o._id, value)}
                           defaultValue={o?.status}
@@ -78,19 +94,30 @@ const AdminOrders = () => {
                               {s}
                             </Option>
                           ))}
-                        </Select>
-                      </td>
-                      <td>{o?.buyer?.name}</td>
-                      <td>{moment(o?.createAt).fromNow()}</td>
-                      <td>{o?.payment.success ? "Success" : "Failed"}</td>
-                      <td>{o?.products?.length}</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div className="container">
+                        </Select>                </td>
+                <td class="px-6 py-4">
+{o?.buyer?.name}               
+ </td>
+                <td class="px-6 py-4">
+ {moment(o?.createAt).fromNow()}
+                </td>
+                <td class="px-6 py-4">
+       {o?.payment.success ? "Success" : "Failed"}
+                </td>
+                <td class="px-6 py-4">
+      {o?.products?.length}
+                </td>
+
+
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+                <div className="container mx-auto ">
                   {o?.products?.map((p, i) => (
-                    <div className="row mb-2 p-3 card flex-row" key={p._id}>
-                      <div className="col-md-4">
+                    <div className="row mb-2 p-3 card flex-row" key={p._id} >
+                      <div className="col-md-4 ">
                         <img
                           src={`/api/v1/product/product-photo/${p._id}`}
                           className="card-img-top"
--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -4,6 +4,7 @@ import Layout from "./../../components/Layout/Layout";
 import axios from "axios";
 import { useAuth } from "../../Context/auth";
 import moment from "moment";
+import UserMenue from "../../components/Layout/UserMenue";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
@@ -21,63 +22,91 @@ const Orders = () => {
     if (auth?.token) getOrders();
   }, [auth?.token]);
   return (
-    <Layout title={"Your Orders"}>
-      <div className="container-flui mt-[6rem] dashboard">
-        <div className="row">
-          <div className="col-md-3">
-            <UserMenu />
-          </div>
-          <div className="col-md-9">
-            <h1 className="text-center">All Orders</h1>
-            {orders?.map((o, i) => {
-              return (
-                <div className="border shadow">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Buyer</th>
-                        <th scope="col"> date</th>
-                        <th scope="col">Payment</th>
-                        <th scope="col">Quantity</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{i + 1}</td>
-                        <td>{o?.status}</td>
-                        <td>{o?.buyer?.name}</td>
-                        <td>{moment(o?.createAt).fromNow()}</td>
-                        <td>{o?.payment.success ? "Success" : "Failed"}</td>
-                        <td>{o?.products?.length}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div className="container">
-                    {o?.products?.map((p, i) => (
-                      <div className="row mb-2 p-3 card flex-row" key={p._id}>
-                        <div className="col-md-4">
-                          <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
-                            className="card-img-top"
-                            alt={p.name}
-                            width="100px"
-                            height={"100px"}
-                          />
-                        </div>
-                        <div className="col-md-8">
-                          <p>{p.name}</p>
-                          <p>{p.description.substring(0, 30)}</p>
-                          <p>Price : {p.price}</p>
-                        </div>
+    <Layout title={"All Orders Data"}>
+      <div className="container-fluid mt-[6rem] flex md:flex-row flex-col">
+        <div className="col-md-3 flex flex-row">
+          <UserMenue />
+        </div>
+        <div className="col-md-9 mt-4 md:mt-0">
+          <h1 className="text-center">All Orders</h1>
+          {orders?.map((o, i) => {
+            return (
+              <div className="border shadow">
+               <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    #
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Buyer
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Payment
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Quantity
+                </th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {i+1}
+                </th>
+                <td class="px-6 py-4">
+                {o?.status}
+            </td>
+                <td class="px-6 py-4">
+{o?.buyer?.name}               
+ </td>
+                <td class="px-6 py-4">
+ {moment(o?.createAt).fromNow()}
+                </td>
+                <td class="px-6 py-4">
+       {o?.payment.success ? "Success" : "Failed"}
+                </td>
+                <td class="px-6 py-4">
+      {o?.products?.length}
+                </td>
+
+
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+                <div className="container mx-auto ">
+                  {o?.products?.map((p, i) => (
+                    <div className="row mb-2 p-3 card flex-row" key={p._id} >
+                      <div className="col-md-4 ">
+                        <img
+                          src={`/api/v1/product/product-photo/${p._id}`}
+                          className="card-img-top"
+                          alt={p.name}
+                          width="100px"
+                          height={"100px"}
+                        />
                       </div>
-                    ))}
-                  </div>
+                      <div className="col-md-8">
+                        <p>{p.name}</p>
+                        <p>{p.description.substring(0, 30)}</p>
+                        <p>Price : {p.price}</p>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </Layout>
